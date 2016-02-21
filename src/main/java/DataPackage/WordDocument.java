dc8dfaff5413f62261548be566d3c999d151c2c5
EGOR <gorodeckiyes@gmail.com>
2016-02-21 19:50:33 +0600

    Create method saveToFile

--- src/main/java/DataPackage/WordDocument.java
+++ src/main/java/DataPackage/WordDocument.java
@@ -1,10 +1,16 @@
 package DataPackage;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 public class WordDocument extends XWPFDocument{
 	
-	public void saveToFile(String nameFile){
-		
+	public void saveToFile(String nameFile) throws IOException{
+		FileOutputStream saveFile = new FileOutputStream(new File(nameFile));
+		super.write(saveFile);
+		saveFile.close();
 	}
 }
