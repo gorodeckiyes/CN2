e2b70757409de891a28c5ce8abf63528be089a95
EGOR <gorodeckiyes@gmail.com>
2016-02-21 19:47:11 +0600

    Create class WordDocument

---
+++ src/main/java/DataPackage/WordDocument.java
@@ -0,0 +1,10 @@
+package DataPackage;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+public class WordDocument extends XWPFDocument{
+	
+	public void saveToFile(String nameFile){
+		
+	}
+}
