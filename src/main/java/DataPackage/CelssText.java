9c8ef7ddfa8d976447b73d35427016f29980f511
EGOR <gorodeckiyes@gmail.com>
2016-02-23 23:58:10 +0600

    cteate test for CelssText

--- src/main/java/DataPackage/CelssText.java
+++ src/main/java/DataPackage/CelssText.java
@@ -9,21 +9,25 @@ public class CelssText {
 	
 	public CelssText(String text) {
 		this();
-		this.setText(text);
+		this.addText(text);
 	}
 	
-	public void setText(String text){
+	public void addText(String text){
 		textCels.append(text);
 	}
 	
+	public String getText(){
+		return textCels.toString();
+	}
+	
 	public void delete(){
-		textCels.delete(0, textCels.length());
+		textCels.delete(0, textCels.toString().length());
 	}
 	
 	public void trim(){
 		String text = textCels.toString();
 		this.delete();
-		this.setText(text);
+		this.addText(text.trim());
 	}
 	
 	public void replaceText(String regex, String replaceText){
