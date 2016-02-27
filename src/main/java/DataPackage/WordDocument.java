7053909a8d92b412fe1388ca540634f7576e7cad
EGOR <gorodeckiyes@gmail.com>
2016-02-27 12:55:29 +0600

    Create comments

--- src/main/java/DataPackage/WordDocument.java
+++ src/main/java/DataPackage/WordDocument.java
@@ -8,6 +8,11 @@ import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 public class WordDocument extends XWPFDocument{
 	
+	/**
+	 * Save result to file
+	 * @param nameFile
+	 * @throws IOException
+	 */
 	public void saveToFile(String nameFile) throws IOException{
 		FileOutputStream saveFile = new FileOutputStream(new File(nameFile));
 		super.write(saveFile);
