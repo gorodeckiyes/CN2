d56ffe7392b5c654e3195ab3a25ee2ef0247342e
EGOR <gorodeckiyes@gmail.com>
2016-02-27 12:43:11 +0600

    create coment class

--- src/main/java/GUI/UserInterface.java
+++ src/main/java/GUI/UserInterface.java
@@ -4,4 +4,9 @@ import javax.swing.JFrame;
 
 public class UserInterface extends JFrame {
 
+	/**
+	 * Class for user interface
+	 */
+	private static final long serialVersionUID = -4301164820427803206L;
+	
 }
