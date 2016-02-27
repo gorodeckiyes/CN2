ec38ec4dace4e5d5f906dc4daf7457862f5b52b1
EGOR <gorodeckiyes@gmail.com>
2016-02-27 23:30:45 +0600

    Create method initialize and method getnamefile and getfilename

--- src/main/java/GUI/UserInterface.java
+++ src/main/java/GUI/UserInterface.java
@@ -1,5 +1,8 @@
 package GUI;
 
+import java.awt.FileDialog;
+import java.io.File;
+
 import javax.swing.JFrame;
 
 public class UserInterface extends JFrame {
@@ -8,5 +11,23 @@ public class UserInterface extends JFrame {
 	 * Class for user interface
 	 */
 	private static final long serialVersionUID = -4301164820427803206L;
+	private String fileName;
+	public void init(){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(50,50);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		this.getFileName();
+	}
 	
+	private void getFileName(){
+		FileDialog fd = new FileDialog(this, "Select file", FileDialog.LOAD);
+		fd.setFile("*.docx");
+		fd.setVisible(true);
+		fileName = fd.getDirectory()+File.separator+fd.getFile();
+	}
+	
+	public String getNameFile(){
+		return fileName;
+	}
 }
