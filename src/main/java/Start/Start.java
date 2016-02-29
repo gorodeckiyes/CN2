579e01e7745f3d5ebe249b8295f60d2c865c55bf
EGOR <gorodeckiyes@gmail.com>
2016-03-01 00:16:14 +0600

    Create method init

--- src/main/java/Start/Start.java
+++ src/main/java/Start/Start.java
@@ -1,5 +1,30 @@
 package Start;
 
-public class Start {
+import java.lang.reflect.InvocationTargetException;
 
+import javax.swing.SwingUtilities;
+
+import GUI.UserInterface;
+
+public class Start {
+	UserInterface ui = null;
+	
+	/**
+	 * Initialize class ui
+	 */
+	public void init(){
+		
+		try {
+			SwingUtilities.invokeAndWait(new Runnable() {
+				
+				@Override
+				public void run() {
+					ui = new UserInterface();
+				}
+			});
+		} catch (InvocationTargetException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
