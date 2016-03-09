4f165830b671f611d095607154fe8dbe608cf607
Egor <gorodeckiyes@gmail.com>
2016-03-09 17:57:18 +0600

    Revert "Create HashMap parametrs"
    This reverts commit 81fbacb9e6b64ea07aaa0ae591fb741faf4dcbe0.

--- src/main/java/MyXML/XMLSave.java
+++ src/main/java/MyXML/XMLSave.java
@@ -1,18 +1,9 @@
 package MyXML;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 public class XMLSave {
 	private String fileName;
-	private HashMap<String, String> parametrs = new HashMap<>();
 	
 	public XMLSave(String fileName) {
 		this.fileName = fileName;
-	}
-	
-	public void saveXML(){
-		
 	}
 }
