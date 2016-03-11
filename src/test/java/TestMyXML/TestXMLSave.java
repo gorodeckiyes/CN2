8df79de978df9ba2c4a19d89c71f7bc949554679
EGOR <gorodeckiyes@gmail.com>
2016-03-12 01:27:27 +0600

    Create class testXMLSave

---
+++ src/test/java/TestMyXML/TestXMLSave.java
@@ -0,0 +1,14 @@
+package TestMyXML;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestXMLSave {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
