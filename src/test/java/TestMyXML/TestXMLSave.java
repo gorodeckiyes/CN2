e02b1ac9326b3643c3cfc16a3799b5efbd660fa1
EGOR <gorodeckiyes@gmail.com>
2016-03-12 01:35:22 +0600

    testSvae

--- src/test/java/TestMyXML/TestXMLSave.java
+++ src/test/java/TestMyXML/TestXMLSave.java
@@ -2,13 +2,24 @@ package TestMyXML;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.Test;
+
+import MyXML.XMLSave;
 
 public class TestXMLSave {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSave() {
+		String FILENAME = "./test.xml";
+		try{
+			XMLSave xmls = new XMLSave(FILENAME);
+			xmls.save();
+				assertTrue(new File(FILENAME).exists());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 }
