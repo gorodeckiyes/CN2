41f11642528e3d090842f4ec0e476fc4780047f7
Egor <gorodeckiyes@gmail.com>
2016-03-25 18:01:51 +0600

    Create method Read

--- src/main/java/MyXML/XMLRead.java
+++ src/main/java/MyXML/XMLRead.java
@@ -1,5 +1,30 @@
 package MyXML;
 
-public class XMLRead {
+import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class XMLRead {
+	String name;
+	ArrayList<String> cells = new ArrayList<>();
+	
+	public XMLRead(String name) {
+		this.name = name;
+	}
+	
+	public ArrayList<String> Read() throws SAXException, IOException, ParserConfigurationException{
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		Document doc = documentBuilder.parse(name);
+		NodeList lNode = doc.getElementsByTagName("options");
+		return null;
+	}
+	
 }
