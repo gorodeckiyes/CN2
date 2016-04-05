b9761cb8936525265aa20768db4395ad65a8db3a
Egor <gorodeckiyes@gmail.com>
2016-04-05 17:20:10 +0600

    optins edit value to method testReaderCells

--- src/test/java/TestMyXML/TestXMLReader.java
+++ src/test/java/TestMyXML/TestXMLReader.java
@@ -15,18 +15,19 @@ import MyXML.XMLRead;
 public class TestXMLReader {
 
 	@Test
-	public void testReader() {
+	public void testReaderCells() {
 		ArrayList<String> optins = null;
 		XMLRead reader = new XMLRead("./options.xml");
 		try {
-			optins = reader.Read();
+			reader.initialize();
 		} catch (SAXException | IOException | ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		optins = reader.Read();
 		int sizeOptins = optins.size();
 		optins.forEach(e -> {System.out.println(e);});
-		assertEquals(sizeOptins, 6);
+		assertEquals(sizeOptins, 4);
 		
 	}
 }
