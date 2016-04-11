c6f1895556395e0f01b948db08eb64a6971c2d04
Egor <gorodeckiyes@gmail.com>
2016-04-11 06:14:27 +0600

    Reader xml file and start class start

--- src/main/java/ru/home/CN2/App.java
+++ src/main/java/ru/home/CN2/App.java
@@ -1,13 +1,47 @@
 package ru.home.CN2;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.xml.sax.SAXException;
+
+import MyXML.XMLRead;
+import MyXML.XMLSave;
+import Start.Start;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static String FILE_NAME = "./options.xml";
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       File file = new File(FILE_NAME);
+       
+       if(!file.exists()){
+    	   try {
+			new XMLSave(FILE_NAME).save();
+		} catch (ParserConfigurationException | TransformerException e) {
+			e.printStackTrace();
+		}   
+       }
+       XMLRead xml = new XMLRead(FILE_NAME);
+       try {
+		xml.initialize();
+       } catch (SAXException | IOException | ParserConfigurationException e) {
+    	   e.printStackTrace();
+       }
+       Start start = null;
+       try {
+		 start = new Start(xml.getCreateReport(), xml.getReadCells());
+       } catch (Exception e) {
+		e.printStackTrace();
+       }
+       start.init();
     }
 }
