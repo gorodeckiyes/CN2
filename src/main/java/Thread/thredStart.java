1491e407a2723e6818126ba714d984384c4e437c
Egor <gorodeckiyes@gmail.com>
2016-04-13 00:22:35 +0600

    add field createReport ad readerCells

--- src/main/java/Thread/thredStart.java
+++ src/main/java/Thread/thredStart.java
@@ -1,7 +1,14 @@
 package Thread;
 
-public class thredStart implements Runnable {
+import java.util.ArrayList;
 
+public class thredStart implements Runnable {
+	private boolean createReport;
+	private ArrayList<String> readerCells;
+	public thredStart(boolean createReport, ArrayList<String> readerCells) {
+		
+	}
+	
 	@Override
 	public void run() {
 		
