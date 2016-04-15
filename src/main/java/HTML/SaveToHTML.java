94ba02a1160be94a17fd629253bd542ca7fe3a70
Egor <gorodeckiyes@gmail.com>
2016-04-15 07:51:36 +0600

    Create class HTML

---
+++ src/main/java/HTML/SaveToHTML.java
@@ -0,0 +1,57 @@
+package HTML;
+
+import java.util.ArrayList;
+
+public class SaveToHTML {
+	private ArrayList<String> htmlBody;
+	
+	public SaveToHTML(String fileName) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void init(){
+		htmlBody = new ArrayList<>();
+	}
+	
+	/**
+	 * Create table
+	 */
+	public void startTable(){
+		
+	}
+	
+	/*
+	 * End Table
+	 */
+	public void endTable(){
+		
+	}
+	
+	/**
+	 * Create row
+	 * @param param - parametr to row
+	 */
+	public void newRow(String param){
+		
+	}
+	
+	public void newcells(String param, String text){
+		
+	}
+	
+	public void addP(String text){
+		
+	}
+	
+	public void addDiv(String param, String text){
+		
+	}
+	
+	public void endFile(){
+		
+	}
+	
+	public void save(){
+		
+	}
+}
