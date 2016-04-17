fc515fd6a3a8b4b1d7cd767ed3730f85eb3e188e
Egor <gorodeckiyes@gmail.com>
2016-04-18 00:45:56 +0600

    add fields readerCells, fileName, cnDoc, cnNewDoc

--- src/main/java/Thread/thredStart.java
+++ src/main/java/Thread/thredStart.java
@@ -1,18 +1,37 @@
 package Thread;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.ArrayList;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+import DataPackage.WordDocument;
 
 public class thredStart implements Runnable {
 	private boolean createReport;
 	private ArrayList<String> readerCells;
-	public thredStart(boolean createReport, ArrayList<String> readerCells) {
-		
+	private File fileName;
+	private WordDocument cnDoc;
+	private WordDocument cnNewDoc;
+	
+	public thredStart(String fileName, boolean createReport, ArrayList<String> readerCells) {
+		this.createReport = createReport;
+		this.readerCells = readerCells;
+		this.fileName = new File(fileName);
 	}
 	
 	@Override
 	public void run() {
 		
-
+		this.ini
+	}
+	
+	/**
+	 * Initialize file
+	 */
+	private void myInit(){
+		cnDoc = (WordDocument) new XWPFDocument(new FileInputStream(fileName));
 	}
 
 }
