e58b6f2c68988bb36ff037d65c0cc64e1c249adc
Egor <gorodeckiyes@gmail.com>
2016-04-22 18:34:36 +0600

    Create method init

--- src/main/java/Thread/thredStart.java
+++ src/main/java/Thread/thredStart.java
@@ -2,11 +2,13 @@ package Thread;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 import DataPackage.WordDocument;
+import HTML.SaveToHTML;
 
 public class thredStart implements Runnable {
 	private boolean createReport;
@@ -14,6 +16,7 @@ public class thredStart implements Runnable {
 	private File fileName;
 	private WordDocument cnDoc;
 	private WordDocument cnNewDoc;
+	private SaveToHTML html;
 	
 	public thredStart(String fileName, boolean createReport, ArrayList<String> readerCells) {
 		this.createReport = createReport;
@@ -23,15 +26,32 @@ public class thredStart implements Runnable {
 	
 	@Override
 	public void run() {
+		this.Init();
 		
-		this.ini
 	}
 	
 	/**
 	 * Initialize file
 	 */
-	private void myInit(){
-		cnDoc = (WordDocument) new XWPFDocument(new FileInputStream(fileName));
+	private void Init(){
+		html = new SaveToHTML(fileName.getPath()+File.separator+"Report_"+fileName.getName()+".html");
+		html.init();
+		try{
+			cnDoc = (WordDocument) new XWPFDocument(new FileInputStream(fileName));
+		}catch(IOException e){
+			html.addP("Not file open file:"+fileName.getPath()+File.separator+fileName.getName());
+			try {
+				if(createReport)
+					html.saveToFile();
+				System.exit(0);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+		cnNewDoc = (WordDocument) new XWPFDocument();
 	}
 
+	private void readerCellsDocument(){
+		
+	}
 }
