bd04fb7938453cdfbae6bc3a1b89510575fce3b5
Egor <gorodeckiyes@gmail.com>
2016-04-28 18:18:12 +0600

    added constructor

--- src/main/java/DataPackage/WordDocument.java
+++ src/main/java/DataPackage/WordDocument.java
@@ -3,11 +3,19 @@ package DataPackage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 public class WordDocument extends XWPFDocument{
 	
+	public WordDocument(InputStream FileName) throws IOException {
+		super(FileName);
+	}
+	
+	public WordDocument() {
+		super();
+	}
 	/**
 	 * Save result to file
 	 * @param nameFile
