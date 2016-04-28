dd1429c4b4bfde402da27588e50f233375791462
Egor <gorodeckiyes@gmail.com>
2016-04-28 18:20:54 +0600

    update method readerCellsTable

--- src/main/java/Thread/thredStart.java
+++ src/main/java/Thread/thredStart.java
@@ -8,8 +8,10 @@ import java.util.Iterator;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
 import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
+import DataPackage.CelssText;
 import DataPackage.WordDocument;
 import HTML.SaveToHTML;
 
@@ -18,28 +20,30 @@ public class thredStart implements Runnable {
 	private ArrayList<String> readerCells;
 	private File fileName;
 	private WordDocument cnDoc;
-	private WordDocument cnNewDoc;
+	private XWPFDocument cnNewDoc;
 	private SaveToHTML html;
 	private int numberRowTable;
 	private int readerCountCells;
-	
+	private int readerCell;
+
 	public thredStart(String fileName, boolean createReport, ArrayList<String> readerCells, int readerCountCells) {
 		this.createReport = createReport;
 		this.readerCells = readerCells;
 		this.fileName = new File(fileName);
 		this.readerCountCells = readerCountCells;
 	}
-	
+
 	@Override
 	public void run() {
 		this.Init();
-		if(cnDoc.getTables().size() > 0){
+		if (cnDoc.getTables().size() > 0) {
 			int tableIndex = 1;
 			Iterator<XWPFTable> tableIterator = cnDoc.getTablesIterator();
-			while(tableIterator.hasNext()){
+			while (tableIterator.hasNext()) {
 				XWPFTable docTable = tableIterator.next();
 				XWPFTable newTable = cnNewDoc.createTable();
-				html.addDiv("Table #"+Integer.toString(tableIndex));
+				html.addDiv("Table #" + Integer.toString(tableIndex));
+				html.newTable("border=\"1\" width=\"100%\" bordercolor=\"black\"");
 				this.readerRowsTable(docTable, newTable);
 				html.endTable();
 				tableIndex++;
@@ -51,62 +55,79 @@ public class thredStart implements Runnable {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		System.exit(0);
 	}
-	
+
 	/**
 	 * Initialize file
 	 */
-	private void Init(){
-		html = new SaveToHTML(fileName.getPath()+File.separator+"Report_"+fileName.getName()+".html");
+	private void Init() {
+		html = new SaveToHTML(fileName.getParent() + File.separator + "Report_" + fileName.getName() + ".html");
 		html.init();
 		this.htmlHat();
 		cnNewDoc = new WordDocument();
-		try{
-			cnDoc = (WordDocument) new XWPFDocument(new FileInputStream(fileName));
-		}catch(IOException e){
-			html.addP("Not file open file:"+fileName.getPath()+File.separator+fileName.getName());
+		try {
+			cnDoc = new WordDocument(new FileInputStream(fileName));
+		} catch (IOException e) {
+			html.addP("Not file open file:" + fileName.getPath() + File.separator + fileName.getName());
 			try {
-				if(createReport)
+				if (createReport)
 					html.saveToFile();
 				System.exit(0);
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
 		}
-		cnNewDoc = (WordDocument) new XWPFDocument();
+		cnNewDoc = new WordDocument();
 	}
 
-	private void readerRowsTable(XWPFTable docTable, XWPFTable newTable){
+	private void readerRowsTable(XWPFTable docTable, XWPFTable newTable) {
 		numberRowTable = 1;
-			docTable.getRows().forEach(row ->{
-				if(createReport)
-					html.newRow();
-					XWPFTableRow newTableRow = newTable.createRow();
-					this.readerCellsTable(numberRowTable, row, newTableRow);
-					newTable.addRow(newTableRow);
-				if(createReport)
-					html.endRow();
-			});
+		docTable.getRows().forEach(row -> {
+			if (createReport)
+				html.newRow();
+			XWPFTableRow newTableRow = newTable.createRow();
+			this.readerCellsTable(numberRowTable, row, newTableRow);
+			newTable.addRow(newTableRow);
+			if (createReport)
+				html.endRow();
+		});
 	}
-	
-	private void readerCellsTable(int rowNumber, XWPFTableRow row, XWPFTableRow newRow){
-		/*if(createReport)
+
+	private void readerCellsTable(int rowNumber, XWPFTableRow row, XWPFTableRow newRow) {
+		if (createReport)
 			html.newCell(Integer.toString(rowNumber), "bgcolor=silver width=10px");
-		for(int i = 0; i < )*/
+		readerCell = 0;
+		row.getTableCells().forEach(cell -> {
+			if (cell.getTextRecursively() != null) {
+				CelssText text = new CelssText(cell.getTextRecursively());
+				if (readerCells.indexOf(Integer.toString(readerCell)) != -1) {
+					XWPFTableCell newCell = newRow.createCell();
+					newCell.setText(text.toString());
+					if (createReport)
+						html.newCell(text.toString(), "bgcolor = #F9F2E3");
+				} else {
+					html.newCell(text.toString(), "bgcolor = red");
+				}
+			} else {
+				html.newCell("", "bgcolor = red  width=10px");
+			}
+			readerCell++;
+		});
 	}
-	
-	private void htmlHat(){
+
+	private void htmlHat() {
 		html.addDiv("Таблица цветов ячеек");
 		html.newTable("border=\"1\" width=\"100%\" bordercolor=\"black\"");
-		
+
 		html.newRow();
-		html.newCell("Такие ячейки будут добавлены","bgcolor = #F9F2E3");
+		html.newCell("Такие ячейки будут добавлены", "bgcolor = #F9F2E3");
 		html.endRow();
-		
+
 		html.newRow();
 		html.newCell("Ячейки с таким цветом не будут добавлены", "bgcolor = red");
 		html.endRow();
-		
+
 		html.newRow();
 		html.newCell("Ячейки с таким цветом для информации, отсутствуют в документе", "bgcolor=silver");
 		html.endRow();
