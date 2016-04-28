b6699f410cdc569199ff16137e50b13cfbbd4d75
Egor <gorodeckiyes@gmail.com>
2016-04-28 12:38:27 +0600

    Used format

--- src/main/java/GUI/UserInterface.java
+++ src/main/java/GUI/UserInterface.java
@@ -12,30 +12,32 @@ public class UserInterface extends JFrame {
 	 */
 	private static final long serialVersionUID = -4301164820427803206L;
 	private String fileName;
-	public void init(){
+
+	public void init() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(50,50);
+		this.setSize(50, 50);
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 		this.showFileDialog();
 	}
-	
+
 	/**
 	 * Create and show FileDialog
 	 */
-	private void showFileDialog(){
+	private void showFileDialog() {
 		FileDialog fd = new FileDialog(this, "Select file", FileDialog.LOAD);
 		fd.setFile("*.docx");
 		fd.setVisible(true);
-		if(!fd.getFile().isEmpty())
-			fileName = fd.getDirectory()+File.separator+fd.getFile();
+		if (!fd.getFile().isEmpty())
+			fileName = fd.getDirectory() + File.separator + fd.getFile();
 	}
-	
+
 	/**
 	 * Get File Name
+	 * 
 	 * @return String
 	 */
-	public String getFileName(){
+	public String getFileName() {
 		return fileName;
 	}
 }
